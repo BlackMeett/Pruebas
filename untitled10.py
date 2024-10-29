843a8e78e63856d77c2d0ac0f60fc8709550ae92
BlackMeett <alejandro.orellana.riquelme@gmail.com>
2024-10-29 15:31:27 -0300

    Update untitled10.py

--- untitled10.py
+++ untitled10.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1ANxMVyiZYPhA8hC2cqhGPCoPLnXcH1ah
 """
 
-import stremlit as st
+import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
