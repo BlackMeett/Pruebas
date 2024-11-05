bf229275e3ce7eff7ce2c1b0a246d7bda75fbbb0
BlackMeett <alejandro.orellana.riquelme@gmail.com>
2024-11-05 16:45:56 -0300

    Update untitled10.py

--- untitled10.py
+++ untitled10.py
@@ -20,7 +20,12 @@ st.write("Galeria De Juegos")
 
 st.title("Forza Horizon 4")
 st.image("Horizon.jpg")
-st.write( "SO: Windows 10 versión 15063.0 o superior Procesador: Intel i7-3820 @ 3.6Ghz Memoria: 8 GB de RAM (12 GB en2) Gráficos: NVidia GTX 970 o NVidia GTX 1060 3GB o AMD R9 290x o AMD RX 470 DirectX: Versión 12")
+st.write("Especificaciones"
+st.write( "SO: Windows 10 versión 15063.0 o superior")
+st.write("Procesador: Intel i7-3820 @ 3.6Ghz")
+st.write("Memoria: 8 GB de RAM (12 GB en2)")
+st.write("Gráficos: NVidia GTX 970 o NVidia GTX 1060 3GB o AMD R9 290x o AMD RX 470")
+st.write("Almacenamiento: 100 GB de espacio disponible")
 
 
 
